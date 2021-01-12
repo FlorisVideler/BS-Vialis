feb71b4a79b27b65042c629d97d1fd645911a41b
Floris Videler <floris.videler@student.hu.nl>
2021-01-12 08:06:31 +0100

    Added test vor agents.py and empty test files for other files.

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,9 +1,9 @@
-from unittest import TestCase
+import unittest
 from GitHub.traffic_model.traffic import agents
 import math
 
 
-class TestGet_next_point(TestCase):
+class TestAgents(unittest.TestCase):
     def test_get_next_point(self):
         distance = 1.0
         p1 = (0, 3)
@@ -12,3 +12,7 @@ class TestGet_next_point(TestCase):
         distance_calculated = math.sqrt(((p1[0] - p2[0]) ** 2) + ((p1[1] - p2[1]) ** 2))
         self.assertEqual(p2, (0.8, 2.4))
         self.assertEqual(distance, distance_calculated)
+
+
+if __name__ == '__main__':
+    unittest.main()
