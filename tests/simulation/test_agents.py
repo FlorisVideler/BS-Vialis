56ddc5714eea1730a717ec3db4bb377ca99b6e25
Expensure <jaspervanloon@outlook.com>
2021-01-18 11:59:04 +0100

    Add doc

--- tests/simulation/test_agents.py
+++ tests/simulation/test_agents.py
@@ -15,6 +15,7 @@ class TestAgents(unittest.TestCase):
         self.assertEqual(distance, distance_calculated)
 
     def test_get_next_node(self):
+        #Tests if current node can find a new node
         test_light = Light(0, Traffic(), (1, 1), False, "Light")
         test_node1 = Node(0, Traffic(), (1, 1), False, False, 5, test_light)
         test_node2 = Node(0, Traffic(), (2, 2), False, False, 5, test_light)
@@ -23,6 +24,7 @@ class TestAgents(unittest.TestCase):
                             "Endnode == Current node, Fails because it cant move.")
 
     def test_red_light(self):
+        #Tests if light is coherent in both node and light itself
         test_light = Light(0, Traffic(), (1, 1), False, "Light")
         test_node = Node(0, Traffic(), (1, 1), False, False, 5, test_light)
 
@@ -33,12 +35,14 @@ class TestAgents(unittest.TestCase):
         self.assertEqual(1, p1, "Stopline and Lightstate are not both False.")
 
     def test_get_light_from_lane(self):
+        #Tests if the sensor can find the activity of a light
         test_model = Traffic()
         test_sensor = Sensor(0, test_model, (1, 1), (0, 1), (2, 1), 0, "Twenty", "1", 1)
         test_model.lanes = test_model.make_intersection()
         self.assertEqual(test_sensor.get_light_from_lane(), test_model.lanes['1']['in']['nodes'][0].light)
 
     def test_get_light_from_lane_no_light(self):
+        #Tests if the lane doesnt have a light, it doesn't show that it has a light
         test_model = Traffic()
         test_sensor = Sensor(0, test_model, (1, 1), (0, 1), (2, 1), 0, "Twenty", "15", 1)
         test_model.lanes = test_model.make_intersection()
--- tests/tools/test_readjson.py
+++ tests/tools/test_readjson.py
@@ -4,17 +4,20 @@ import json, unittest
 
 class TestAgents(unittest.TestCase):
     def test_normalize(self):
+        #Tests if normalisation works according to formula.
         p1 = tr.normalize([8, 4, 3, 2, 7, 6, 5])
         p2 = tr.normalize([1, 2, 2, 3, 3, 3, 4])
         self.assertEqual(p1, [1, 1 / 3, 1 / 6, 0, 5 / 6, 4 / 6, 1 / 2], "Normalisatie is incorrect")
         self.assertEqual(p2, [0, 1 / 3, 1 / 3, 2 / 3, 2 / 3, 2 / 3, 1], "Normalisatie is incorrect")
 
     def test_convert_time(self):
+        #Tests if the time converter works and gives the right time.
         p1 = tr.convert_time('2021-01-08T12:18:47Z')
         p2 = '08-01-2021 13:18:47.000000'
         self.assertEqual(p1, p2, "Time conversion is incorrect")
 
     def test_path(self):
+        #Tests if *a* path works
         p1 = 1
         subject_path = tr.true_path('laneset_BOS210.json')
         try:
