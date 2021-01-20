570fbba87d4b5131bb6256ddc0f113cf5cb86449
Kai ter Horst <Kai.terhorst@student.hu.nl>
2021-01-20 13:37:08 +0100

    changed documentation format

--- tools/XML_tool.py
+++ tools/XML_tool.py
@@ -9,12 +9,12 @@ def load_data():
     return root
 
 
-def format_lat_lon(la, lo):
+def format_lat_lon(la: str, lo: str) -> tuple:
     """
     This function transforms the latitude and longitude into a usuable format
-    :param la: int
-    :param lo: int
-    :return: (float, float)
+    :param la: latitude
+    :param lo: longitude
+    :return: (latitude, longitude)
     """
 
     la = la[:2] + '.' + la[2:]
@@ -22,11 +22,11 @@ def format_lat_lon(la, lo):
     return la, lo
 
 
-def lane_type(lane_attributes):
+def lane_type(lane_attributes: str) -> str:
     """
     This function retrieves the lane type.
-    :param lane_attributes: string,
-    :return: type_lane: string
+    :param lane_attributes: location to find info about the lane including lanetype
+    :return: type_lane: shows who is allowed to use that lane
     """
 
     type_lane = None
@@ -46,12 +46,12 @@ def lane_type(lane_attributes):
     return type_lane
 
 
-def func_connects_to(connects_to, signal_dict):
+def func_connects_to(connects_to: str, signal_dict: dict) -> dict:
     """
     This function retrieves and adds the connection_id to the dict.
-    :param connects_to: string
-    :param signal_dict: dictionary
-    :return: connects_to_dict: dictionary
+    :param connects_to: info about the relations between lanes
+    :param signal_dict: different signal groups
+    :return: connects_to_dict: information like lane, signalgroup and connections
     """
     connection = connects_to.find('connection')
     connecting_lane = connection.find('connectingLane')
@@ -68,11 +68,11 @@ def func_connects_to(connects_to, signal_dict):
     return connects_to_dict
 
 
-def func_regional(regional):
+def func_regional(regional: str) -> list:
     """
     This function retrieves the latitude and longitude in regional.
-    :param regional: string
-    :return: nodes_regional_list: list
+    :param regional: nodes
+    :return: nodes_regional_list: list of nodes
     """
     add_grp_c = regional.find('addGrpC')
     nodes = add_grp_c.find('nodes')
@@ -89,22 +89,22 @@ def func_regional(regional):
     return nodes_regional_list
 
 
-def func_sensor_position(sensor_position):
+def func_sensor_position(sensor_position: str) -> tuple:
     """
     This function retrieves the starting and ending position of a specific sensor and formats it.
-    :param sensor_position: string
-    :return: sensor_position: (string, string)
+    :param sensor_position: latitude and longitude
+    :return: sensor_position: (x and y position)
     """
     lat, long = sensor_position.find('lat').text, sensor_position.find('long').text
     lat, long = format_lat_lon(lat, long)
     sensor_position = (lat, long)
     return sensor_position
 
-def func_real_sensor_position(geo_shape):
+def func_real_sensor_position(geo_shape: str) -> tuple:
     """
     This function retrieves the sensor position using the geoshape, (extra precise)
-    :param geo_shape: string
-    :return: real_sensor_position: (('string', 'string'), ('string', 'string'))
+    :param geo_shape: outline of the sensor
+    :return: real_sensor_position: pairs of latitudes and longitudes
     """
     start_sensor = (format_lat_lon(geo_shape[1].find('lat').text, geo_shape[1].find('long').text))
     end_sensor = (format_lat_lon(geo_shape[2].find('lat').text, geo_shape[2].find('long').text))
